05f3eb45e0b76681ff003e9548c27c919c38f793
nachiketshilwant <91380338+nachiketshilwant@users.noreply.github.com>
2022-08-13 21:56:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+import UseState from './component/Hooks/useState'
+import UseEffect from './component/Hooks/useEffect'
+import UseReducer from './component/Hooks/useReducer'
+
+
+const App = () => {
+  return(
+    // <UseState />
+    <UseEffect />
+    // <UseReducer />
+
+  )
+}
+
+export default App
